bbcd655b6aab7c4cedce4e53a36025f4f97e4989
Jordan Rand <jorrand@gmail.com>
2018-10-01 10:19:20 -0600

    function to controllers

--- app/controllers/api/ingredients_controller.rb
+++ app/controllers/api/ingredients_controller.rb
@@ -1,4 +1,44 @@
 class Api::IngredientsController < ApplicationController
+  before_action: set_indredient, only: [:show, :update, :destroy]
+
   def index
+    render json: Ingredient.all
   end
+
+  def show
+    render json: @Ingredient
+  end
+
+  def create
+    ingredient = Ingredient.new(ingredient_params)
+
+    if ingredient.save
+      render json: ingredient
+    else
+      render_error(ingredient)
+    end
+  end
+
+  def update
+    if @ingredient.update(ingredient_params)
+      render json: @ingredient
+    else
+      render_error(@ingredient)
+    end
+  end
+
+  def destroy
+    @ingredient.destroy
+  end
+
+  private
+
+    def set_indredient
+      @ingredient = Ingredient.find(params[:id])
+    end
+
+    def ingredient_params
+      params.require(:ingredient).permit(:name)
+    end
+    
 end
