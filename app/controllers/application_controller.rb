bbe202081be5b8b399ac62de3c0139395eabcd86
David Jungst <djungst@gmail.com>
2018-10-01 09:34:58 -0600

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
+<<<<<<< HEAD
   def render_error(obj)
     render json: { errors: obj.errors.full_messages }, status: 422
   end
+=======
+>>>>>>> initial commit
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
+<<<<<<< HEAD
     resources :ingredients
     resources :recipes
     resources :recipe_ingredients, only: [:create, :destroy, :new]
+=======
+    #API ROUTES SHOULD GO HERE
+>>>>>>> initial commit
   end
 
   #Do not place any routes below this one
